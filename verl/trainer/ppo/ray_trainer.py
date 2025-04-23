ee7581b1e35e29f88573a265cdd4dec739dbab41
paulinebourigault <pbourigault@gmail.com>
2025-04-23 14:15:07 +0000

    update ppo/ray_trainer, ppo/reward

--- verl/trainer/ppo/ray_trainer.py
+++ verl/trainer/ppo/ray_trainer.py
@@ -1172,9 +1172,9 @@ class RayPPOTrainer:
                             batch = batch.union(reward_tensor)
 
                         if self.config.reward_model.launch_reward_fn_async:
-                            future_reward = compute_reward_async.remote(batch, self.config, self.tokenizer)
+                            future_reward = compute_reward_async.remote(batch, self.config, self.tokenizer, tracker=self.tracker, step=self.global_steps)
                         else:
-                            reward_tensor, reward_extra_infos_dict = compute_reward(batch, self.reward_fn)
+                            reward_tensor, reward_extra_infos_dict = compute_reward(batch, self.reward_fn, tracker=self.tracker, step=self.global_steps)
 
                     # recompute old_log_probs
                     with marked_timer("old_log_prob", timing_raw, color="blue"):
@@ -1234,19 +1234,7 @@ class RayPPOTrainer:
                             reward_tensor, reward_extra_infos_dict = ray.get(future_reward)
                         batch.batch["token_level_scores"] = reward_tensor
 
-                        if self.config.trainer.log_rollout_generations > 0:
-                            # Extract inputs, outputs and scores from the batch
-                            input_ids = batch.batch['input_ids']
-                            input_texts = [self.tokenizer.decode(ids, skip_special_tokens=True) for ids in input_ids]
-                            output_ids = batch.batch['responses']
-                            output_texts = [self.tokenizer.decode(ids, skip_special_tokens=True) for ids in output_ids]
-                            scores = reward_tensor.sum(-1).cpu().tolist()
-                            self._log_all_generations(inputs=input_texts,
-                                                      outputs=output_texts,
-                                                      scores=scores,
-                                                      epoch=epoch)
-
-                        print(f'{list(reward_extra_infos_dict.keys())=}')
+                        print(f"{list(reward_extra_infos_dict.keys())=}")
                         if reward_extra_infos_dict:
                             batch.non_tensor_batch.update({k: np.array(v) for k, v in reward_extra_infos_dict.items()})
 
