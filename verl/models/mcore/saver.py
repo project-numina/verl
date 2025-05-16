0732e9a5bafed5f39245c9385ed90a81790b004e
ShareLer <48175490+ShareLer@users.noreply.github.com>
2025-05-16 10:21:57 +0800

    [megatron] fix head_dim in GQA model when load from hf ckpt (#1513)
    - [x] Search for similar PR(s).
    
    fix head_dim in GQA model when load from hf ckpt
    
    > Demonstrate the high-level design if this PR is complex.
    
    - Change the acquisition methods of q and kv head_dim to be compatible
    with GQA.
    - Add the conversions of q_layernorm and k_layernorm in
    convert_megatron_model_to_transformers_model for Qwen3.
    
    > Demonstrate how the API changes if any.
    
    > Provide usage example(s) for easier usage.
    
    ```python
    ```
    
    > For changes that can not be tested by CI (e.g., algorithm
    implementation, new model support), validate by experiment(s) and show
    results like training curve plots, evaluatuion results, etc.
    
    - **Issue Number**: Fixes issue #1510
    
    - [x] Read the [Contribute
    Guide](https://github.com/volcengine/verl?tab=readme-ov-file#contribution-guide).
    - [x] Apply [pre-commit
    checks](https://github.com/volcengine/verl?tab=readme-ov-file#code-linting-and-formatting).
    - [ ] Add `[BREAKING]` to the PR title if it breaks any API.
    - [ ] Update the documentation about your changes in the
    [docs](https://github.com/volcengine/verl/tree/main/docs).
    - [x] Add CI test(s) if neccessary.
    
    ---------
    
    Signed-off-by: ShareLer <ShareLe@163.com>

--- verl/models/mcore/saver.py
+++ verl/models/mcore/saver.py
@@ -381,7 +381,7 @@ def merge_megatron_ckpt_gptmodel(wrapped_models, config, dtype, is_value_model=F
                     f"{layer_name}.self_attn.k_norm.weight",
                     src_pp_rank=src_pp_rank,
                 )
-
+                
             _broadcast_tp_shard_tensor_qkv(
                 sync_layer.self_attention.linear_qkv.weight,
                 f"{layer_name}.self_attn.q_proj.weight",
