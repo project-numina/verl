f1815c833bdf78eceb77388fb6b44773267b861c
Marco Dos Santos <92208507+marco-dossantos@users.noreply.github.com>
2025-05-25 04:10:26 +0100

    Small changes for multi-turn rl (#27)
    This PR now:
    - Reverts changes by @marco-dossantos in verl/verl/protocol.py
    - Include both extra info and turn info in batch reward manager.
    - Fixes a bug that prevents from detokenizing multi-turn conversation correctly in `ray_trainer.py`
    - Sets timeout to None in new async server to allow long waits for long token completions.
    - Fixes a bug where filter overlong prompt is not based on token length but pure length (I think token length is better as design choice)

--- verl/workers/reward_manager/batch.py
+++ verl/workers/reward_manager/batch.py
@@ -18,10 +18,14 @@ import torch
 
 from verl import DataProto
 
+<<<<<<< HEAD
 from verl.workers.reward_manager import register
 
 
 @register("batch")
+=======
+
+>>>>>>> 0772822 (Small changes for multi-turn rl (#27))
 class BatchRewardManager:
     """
     A batch reward manager that computes rewards for a batch of data.
@@ -59,12 +63,14 @@ class BatchRewardManager:
         ground_truths = [item.non_tensor_batch["reward_model"].get("ground_truth", None) for item in data]
         data_sources = data.non_tensor_batch[self.reward_fn_key]
         extras = data.non_tensor_batch.get("extra_info", [None] * len(data))
+        turns = data.non_tensor_batch.get("turn_info", [None] * len(data))
 
         scores = self.compute_score(
             data_sources=data_sources,
             solution_strs=responses_str,
             ground_truths=ground_truths,
             extra_infos=extras,
+            turn_infos=turns,
             **self.reward_kwargs,
         )
 
