acf4fedc528122a3679502c3e00f92407efcb2b0
Thibaut Barroyer <t.barroyer@icloud.com>
2025-05-27 21:39:50 +0200

    wip

--- verl/trainer/ppo/SIL.py
+++ verl/trainer/ppo/SIL.py
@@ -1,21 +1,7 @@
-# Copyright 2025 Individual Contributor: Thibaut Barroyer
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import os
 import pickle
 import random
-from collections import defaultdict, deque
+from collections import defaultdict
 from typing import Dict
 
 from verl import DataProto
@@ -23,7 +9,7 @@ from verl import DataProto
 
 class RolloutDatabase:
     """
-    Stores last k successfull rollouts for every prompts.
+    Stores last k successful rollouts for every prompt.
     Can be used for replay or self-imitation learning.
     Can persist to disk and reload lazily using memory-mapped files if needed.
     """
@@ -36,19 +22,17 @@ class RolloutDatabase:
         """
         self.k = k
         self.reward_threshold = reward_threshold
-        self._buckets: Dict[str, dict] = defaultdict(lambda: deque(maxlen=self.k))
-        pass
+        self._buckets: Dict[str, list] = defaultdict(list)
 
     def add(self, rollout_batch: DataProto):
         """
         Add a batch of rollouts to the database.
         Args:
-            batch (DataProto): Batch of rollouts to add.
+            rollout_batch (DataProto): Batch of rollouts to add.
         """
         for idx in range(len(rollout_batch)):
             rollout_item = rollout_batch[idx]
             prompt_idx = rollout_item.non_tensor_batch["index"]
-            # Check if the reward is above the threshold
             if rollout_item.batch["acc"] >= self.reward_threshold:
                 item = {
                     "responses": rollout_item.batch["responses"],
@@ -58,7 +42,12 @@ class RolloutDatabase:
                     "nt_acc": rollout_item.non_tensor_batch["acc"],
                     "nt_score": rollout_item.non_tensor_batch["score"],
                 }
-                self._buckets[prompt_idx].append(item)
+                bucket = self._buckets[prompt_idx]
+                bucket.append(item)
+                if len(bucket) > self.k:
+                    bucket.pop(0)  # Remove oldest
+
+                print(f"Added rollout for prompt {prompt_idx}: {item['nt_acc']} in bucket of size {len(bucket)}")
 
     def replace_one_if_all_failed(self, rollout_batch: DataProto):
         """
@@ -74,23 +63,19 @@ class RolloutDatabase:
         """
         ids_to_recompute = []
         ids_to_keep = []
-
-        # group all rollouts by prompt index
         prompt_to_indices = defaultdict(list)
+
         for idx in range(len(rollout_batch)):
             prompt_idx = rollout_batch[idx].non_tensor_batch["index"]
             prompt_to_indices[prompt_idx].append(idx)
 
         for prompt_idx, indices in prompt_to_indices.items():
-            # Check if all responses for that prompt are below the threshold
             all_below_threshold = all(rollout_batch[i].batch["acc"] < self.reward_threshold for i in indices)
 
             if all_below_threshold and self._buckets[prompt_idx]:
-                # Replace only one of them with a successful sample
                 to_replace_idx = indices[0]
-                replacement = random.choice(list(self._buckets[prompt_idx]))
+                replacement = random.choice(self._buckets[prompt_idx])
 
-                # Only keep minimal data
                 rollout_batch.batch["responses"][to_replace_idx] = replacement["responses"]
                 rollout_batch.batch["response_mask"][to_replace_idx] = replacement["response_mask"]
                 rollout_batch.batch["acc"][to_replace_idx] = replacement["acc"]
@@ -100,10 +85,8 @@ class RolloutDatabase:
                 rollout_batch.non_tensor_batch["acc"][to_replace_idx] = replacement["nt_acc"]
 
                 ids_to_recompute.append(to_replace_idx)
-                # Keep the rest
                 ids_to_keep.extend(indices[1:])
             else:
-                # Keep all original rollouts
                 ids_to_keep.extend(indices)
 
         return ids_to_recompute, ids_to_keep
@@ -116,8 +99,7 @@ class RolloutDatabase:
             filepath (str): Path to the file where the database should be saved.
         """
         with open(filepath, "wb") as f:
-            serializable_buckets = {k: list(v) for k, v in self._buckets.items()}
-            pickle.dump(serializable_buckets, f)
+            pickle.dump(self._buckets, f)
 
     def load(self, filepath: str):
         """
@@ -131,6 +113,4 @@ class RolloutDatabase:
 
         with open(filepath, "rb") as f:
             data = pickle.load(f)
-            self._buckets = defaultdict(lambda: deque(maxlen=self.k))
-            for k, v in data.items():
-                self._buckets[k] = deque(v, maxlen=self.k)
+            self._buckets = defaultdict(list, {k: v[: self.k] for k, v in data.items()})
