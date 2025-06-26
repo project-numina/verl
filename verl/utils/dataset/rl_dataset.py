b10bf7b053b29cd8bd0dda1e65516668d43c1764
vinyesm <vinyes.marina@gmail.com>
2025-06-26 17:34:54 +0200

    fix serialization

--- verl/utils/dataset/rl_dataset.py
+++ verl/utils/dataset/rl_dataset.py
@@ -19,6 +19,7 @@ import logging
 import os
 import re
 from collections import defaultdict
+from functools import partial
 from typing import List, Optional, Union
 
 import datasets
@@ -64,6 +65,8 @@ def collate_fn(data_list: list[dict]) -> dict:
 
     return {**tensors, **non_tensors}
 
+def filter_prompt(doc, tokenizer, prompt_key, max_prompt_length):
+    return len(tokenizer.apply_chat_template(doc[prompt_key], add_generation_prompt=True, tokenize=True)) < max_prompt_length
 
 class RLHFDataset(Dataset):
     """
@@ -96,7 +99,6 @@ class RLHFDataset(Dataset):
         self.original_data_files = copy.deepcopy(data_files)  # use for resume
         self.tokenizer = tokenizer
         self.processor = processor
-        self.config = config
 
         self.cache_dir = os.path.expanduser(config.get("cache_dir", "~/.cache/verl/rlhf"))
         self.prompt_key = config.get("prompt_key", "prompt")
@@ -110,7 +112,6 @@ class RLHFDataset(Dataset):
 
         self.num_workers = config.get("filter_overlong_prompts_workers", max(1, os.cpu_count() // 4))
         self.num_workers = min(self.num_workers, os.cpu_count())
-        self.use_shm = config.get("use_shm", False)
         self.chat_template_func = config.get("chat_template_func", None)
         self.need_tools_kwargs = config.get("need_tools_kwargs", False)
         self.filter_prompts = config.get("filter_prompts", True)
@@ -123,7 +124,7 @@ class RLHFDataset(Dataset):
 
         data_files = self.data_files if not use_origin_parquet else self.original_data_files
         for i, parquet_file in enumerate(data_files):
-            self.data_files[i] = copy_to_local(src=parquet_file, cache_dir=self.cache_dir, use_shm=self.use_shm)
+            self.data_files[i] = copy_to_local(src=parquet_file, cache_dir=self.cache_dir)
 
     def _read_files_and_tokenize(self):
         dataframes = []
@@ -135,37 +136,32 @@ class RLHFDataset(Dataset):
 
         print(f"dataset len: {len(self.dataframe)}")
 
-        # filter out too long prompts
         if self.filter_overlong_prompts:
-            tokenizer = self.tokenizer
-            processor = self.processor
-            prompt_key = self.prompt_key
-            image_key = self.image_key
-            video_key = self.video_key
-
-            if processor is not None:
-                from verl.utils.dataset.vision_utils import process_image, process_video
-
-                def doc2len(doc) -> int:
-                    messages = self._build_messages(doc)
-                    raw_prompt = self.processor.apply_chat_template(messages, add_generation_prompt=True, tokenize=False)
-                    images = [process_image(image) for image in messages.pop(image_key)] if image_key in messages else None
-                    videos = [process_video(video) for video in messages.pop(video_key)] if video_key in messages else None
-
-                    return len(processor(text=[raw_prompt], images=images, videos=videos)["input_ids"][0])
-
-            else:
-
-                def doc2len(doc) -> int:
-                    return len(tokenizer.apply_chat_template(doc[prompt_key], add_generation_prompt=True))
-
+            filter_fn = partial(
+                filter_prompt,
+                tokenizer=self.tokenizer,
+                prompt_key=self.prompt_key,
+                max_prompt_length=self.max_prompt_length,
+            )
+            
             self.dataframe = self.dataframe.filter(
-                lambda doc: len(tokenizer.apply_chat_template(doc[prompt_key], add_generation_prompt=True, tokenize=True)) < self.max_prompt_length,
+                filter_fn,
                 num_proc=self.num_workers,
                 desc=f"Filtering prompts longer than {self.max_prompt_length} tokens",
             )
-
             print(f"filter dataset len: {len(self.dataframe)}")
+
+        # # filter out too long prompts
+        # if self.filter_overlong_prompts:
+        #     tokenizer = self.tokenizer
+        #     prompt_key = self.prompt_key
+        #     self.dataframe = self.dataframe.filter(
+        #         lambda doc: len(tokenizer.apply_chat_template(doc[prompt_key], add_generation_prompt=True, tokenize=True)) < self.max_prompt_length,
+        #         num_proc=self.num_workers,
+        #         desc=f"Filtering prompts longer than {self.max_prompt_length} tokens",
+        #     )
+
+        #     print(f"filter dataset len: {len(self.dataframe)}")
 
     def resume_dataset_state(self):
         self.serialize_dataset = not hasattr(self, "original_data_files")
@@ -186,9 +182,7 @@ class RLHFDataset(Dataset):
             for message in messages:
                 content = message["content"]
                 content_list = []
-                segments = re.split("(<image>|<video>)", content)
-                segments = [item for item in segments if item != ""]
-                for segment in segments:
+                for segment in re.split("(<image>|<video>)", content):
                     if segment == "<image>":
                         content_list.append({"type": "image"})
                     elif segment == "<video>":
@@ -215,12 +209,12 @@ class RLHFDataset(Dataset):
             multi_modal_data = {}
 
             images = None
-            if self.image_key in row_dict and row_dict.get(self.image_key, None) is not None:
+            if self.image_key in row_dict:
                 images = [process_image(image) for image in row_dict.pop(self.image_key)]
                 multi_modal_data["image"] = images
 
             videos = None
-            if self.video_key in row_dict and row_dict.get(self.video_key, None) is not None:
+            if self.video_key in row_dict:
                 videos = [process_video(video) for video in row_dict.pop(self.video_key)]
                 multi_modal_data["video"] = [video.numpy() for video in videos]
 
@@ -254,7 +248,7 @@ class RLHFDataset(Dataset):
             truncation=self.truncation,
         )
 
-        if self.processor is not None and "Qwen2VLImageProcessor" in self.processor.image_processor.__class__.__name__:
+        if self.processor is not None and self.processor.image_processor.__class__.__name__ == "Qwen2VLImageProcessor":
             from verl.models.transformers.qwen2_vl import get_rope_index
 
             position_ids = [
@@ -300,13 +294,11 @@ class RLHFDataset(Dataset):
         # add index for each prompt
         index = row_dict.get("extra_info", {}).get("index", 0)
         tools_kwargs = row_dict.get("extra_info", {}).get("tools_kwargs", {})
-        interaction_kwargs = row_dict.get("extra_info", {}).get("interaction_kwargs", {})
         need_tools_kwargs = row_dict.get("extra_info", {}).get("need_tools_kwargs", self.need_tools_kwargs)
         if need_tools_kwargs and not tools_kwargs:
             logger.warning("tools_kwargs is empty for index {}, data source: {}", index, row_dict["data_source"])
         row_dict["index"] = index
         row_dict["tools_kwargs"] = tools_kwargs
-        row_dict["interaction_kwargs"] = interaction_kwargs
         return row_dict
 
     def __getstate__(self):
