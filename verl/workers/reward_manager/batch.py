79a1b8acf14f5801704770c52ef010c2f8aaacfe
vinyesm <vinyes.marina@gmail.com>
2025-06-29 21:58:00 +0200

    logger instead of print

--- verl/workers/reward_manager/batch.py
+++ verl/workers/reward_manager/batch.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 from collections import defaultdict
+import logging
 
 import torch
 
@@ -20,6 +21,8 @@ from verl import DataProto
 
 from verl.workers.reward_manager import register
 
+logger = logging.getLogger(__file__)
+logger.setLevel(os.getenv("VERL_LOGGING_LEVEL", "WARN"))
 
 @register("batch")
 
@@ -75,9 +78,9 @@ class BatchRewardManager:
 
     def __call__(self, data: DataProto, return_dict=False):
         # If there is rm score, we directly return rm score. Otherwise, we compute via rm_score_fn
-        print("MARINA DEBUG: data.batch.keys(): ", data.batch.keys())
+        logger.warning(f"MARINA DEBUG: data.batch.keys(): {data.batch.keys()}")
         if "rm_scores" in data.batch.keys():
-            print("MARINA DEBUG: rm_scores found in data batch, returning directly.")
+            logger.warning("MARINA DEBUG: rm_scores found in data batch, returning directly.")
             if return_dict:
                 return {"reward_tensor": data.batch["rm_scores"]}
             else:
@@ -100,16 +103,16 @@ class BatchRewardManager:
             score = scores[i]
 
             if isinstance(score, dict):
-                print("MARINA DEBUG: Score is a dict, extracting reward and extra info.")
-                print("MARINA DEBUG: type(score): ", type(score))
-                print("MARINA DEBUG: score keys: ", score.keys())
-                print("MARINA DEBUG: score['score']: ", score["score"])
+                logger.warning("MARINA DEBUG: Score is a dict, extracting reward and extra info.")
+                logger.warning(f"MARINA DEBUG: type(score): {type(score)}")
+                logger.warning(f"MARINA DEBUG: score keys:  {score.keys()}")
+                logger.warning(f"MARINA DEBUG: score['score']: {score["score"]}")
                 reward = score["score"]
                 for key, value in score.items():
                     reward_extra_info[key].append(value)
             else:
-                print("MARINA DEBUG: Score is not a dict, extracting reward and extra info.")
-                print("MARINA DEBUG: score: ", score)
+                logger.warning(f"MARINA DEBUG: Score is not a dict, extracting reward and extra info.")
+                logger.warning(f"MARINA DEBUG: score:  {score}")
                 reward = score
 
             rewards.append(reward)
