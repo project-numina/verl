d5f357961f6b1099b016c59d669f82e94ab457fe
Hugues de Saxcé <hugues.de.saxce@protonmail.com>
2025-07-01 18:28:17 +0200

    fix: remove duplicate advantage estimator

--- verl/trainer/ppo/ray_trainer.py
+++ verl/trainer/ppo/ray_trainer.py
@@ -76,20 +76,6 @@ class Role(Enum):
     ActorRolloutRef = 6
 
 
-class AdvantageEstimator(str, Enum):
-    """
-    Using an enumeration class to avoid spelling errors in adv_estimator
-    """
-
-    GAE = "gae"
-    GRPO = "grpo"
-    REINFORCE_PLUS_PLUS = "reinforce_plus_plus"
-    REINFORCE_PLUS_PLUS_BASELINE = "reinforce_plus_plus_baseline"
-    REMAX = "remax"
-    RLOO = "rloo"
-    GRPO_PASSK = "grpo_passk"
-
-
 @dataclass
 class ResourcePoolManager:
     """
