edc2dd3639f9e6b24f1248b66f7c78e7c55a79f2
Thibaut Barroyer <t.barroyer@icloud.com>
2025-07-03 23:02:10 +0200

    fix np array ?

--- verl/trainer/ppo/ray_trainer.py
+++ verl/trainer/ppo/ray_trainer.py
@@ -51,7 +51,7 @@ from verl.trainer.ppo.metric_utils import (
 )
 from verl.trainer.ppo.reward import compute_reward, compute_reward_async
 from verl.trainer.ppo.SIL import RolloutDatabase
-from verl.utils.checkpoint.checkpoint_manager import BaseCheckpointManager, find_latest_ckpt_path
+from verl.utils.checkpoint.checkpoint_manager import find_latest_ckpt_path, should_save_ckpt_esi
 
 from verl.utils.debug import marked_timer
 from verl.utils.metric import (
